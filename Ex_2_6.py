17224249e99ccaf86c959b563498f3a90e237131
Evgeniy <evgeniyskachkov97@gmail.com>
2020-11-06 15:52:02 +0500

    Update Ex_2_6.py

--- Ex_2_6.py
+++ Ex_2_6.py
@@ -1,1 +1,28 @@
+items = []
 
+id = 0
+name_ = ""
+cost_ = 0
+count_ = 0
+ed_ = ""
+
+analitics_ = {"название": [], "стоимость": [], "количество": [], "ед.": []}
+
+n = int(input("введите количество товара: "))
+i = 0
+while i < n:
+    name_ = input("Введите название товара: ")
+    analitics_["название"].append(name_)
+    cost_ = input("Введите стоимость товара: ")
+    analitics_["стоимость"].append(cost_)
+    count_ = input("Введите количество товара: ")
+    analitics_["количество"].append(count_)
+    ed_ = input("Введите единицу измерения: ")
+    analitics_["ед."].append(ed_)
+    items.append((id, {"название": name_, "стоимость": cost_, "количество": count_, "ед.": ed_}))
+    id += 1
+    i += 1
+
+for id, item in enumerate(items):
+    print(item)
+print(analitics_)
