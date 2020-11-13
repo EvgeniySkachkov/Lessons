eb72509b3bf7992102fd6e9cf730a7961303193a
Evgeniy <evgeniyskachkov97@gmail.com>
2020-11-13 21:38:19 +0600

    Lesson_4

--- Ex_4_5.py
+++ Ex_4_5.py
@@ -1,6 +1,6 @@
 from functools import reduce
 
-list_ = [el for el in range(100, 1001) if el % 2 ==0]
+list_ = [el for el in range(100, 1001) if el % 2 == 0]
 print(list_)
 multiply = (reduce(lambda prev_el, el: prev_el * el, list_))
 print(multiply)
